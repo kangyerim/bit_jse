ba79a8fe5be3c957a29ea7b15b06481f15672a90
kangyerim <yerimkang521@gmail.com>
2020-04-08 11:55:12 +0900

    200408 11:52

---
+++ inheritance/Main.java
@@ -0,0 +1,16 @@
+package com.jse.inheritance;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.println("(0)종료   (1)시작");
+			switch(scanner.nextInt()) {
+			case 0:
+				return;
+			case 1:
+				break;
+			}
+		}
+	}
+}
